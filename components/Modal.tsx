f261620190a134cf96bded4ba02484902008f607
RexCloud <kf567rava@gmail.com>
2023-09-23 16:37:09 +0500

    Use style prop for modal component styling

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -10,6 +10,7 @@ const Modal = ({ visible, onClose, children }: { visible: Boolean, onClose: Func
         document.removeEventListener("keydown", keyDownHandler)
         onClose()
     }
+    
     useEffect(() => {
         if (visible) document.addEventListener("keydown", keyDownHandler)
         return () => {
@@ -26,8 +27,8 @@ const Modal = ({ visible, onClose, children }: { visible: Boolean, onClose: Func
         onClose()
     }
 
-    return <div className="fixed inset-0 bg-black bg-opacity-30 text-slate-50 text-lg flex justify-center items-center" id="wrapper" onClick={handleClose}>
-        <div className="bg-gray-700 flex flex-col justify-center p-6 rounded-xl">
+    return <div style={{position: "fixed", inset: 0, background: "rgba(0, 0, 0, 0.3)", zIndex: 1, color: "rgb(248, 250, 252)", fontSize: "18px", display: "flex", justifyContent: "center", alignItems: "center"}} id="wrapper" onClick={handleClose}>
+        <div style={{background: "rgb(55, 65, 81)", display: "flex", flexDirection: "column", justifyContent: "center", padding: 28, borderRadius: 12}}>
             {children}
         </div>
     </div>
